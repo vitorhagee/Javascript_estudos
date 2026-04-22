9758bd04b4fa05cc8435e8d20999d2b044d81332
vitor hagee <vitorhagee@gmail.com>
2026-04-22 12:51:33 -0300

    começo da API estudo

--- Index.js
+++ Index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-app.use(express.json());
+app.get('/',(req,res) => {
+    res.send('API rodando');
+});
+
+app.listen(3001, () => {
+    console.log('servidor rodando com sucesso!');
+});
+
+app.get('/main', (req, res) => {
+  res.json({ status: 'chegando...' });
+});

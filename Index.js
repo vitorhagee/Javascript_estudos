f84962e826d0246721a8644d53838e8c63481ec0
vitor hagee <vitorhagee@gmail.com>
2026-04-22 17:29:04 -0300

    Adicionado novo post e get

--- Index.js
+++ Index.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const users = [];
 
 const app = express();
 const PORT = 3001;
+app.use(express.json());
 
 app.get('/',(req,res) => {
     res.send('API rodando');
 });
 
-app.listen(3001, () => {
-    console.log('servidor rodando com sucesso!');
-});
-
 app.get('/main', (req, res) => {
   res.json({ status: 'chegando...' });
+});
+
+app.post('/users', (req,res) =>{
+
+    const user = req.body
+
+    users.push(user);
+    res.status(201).json({
+        message: 'Usuário Criado',
+        data: user
+    })
+})
+
+app.get('/users',(req,res) => {
+    res.json(users);
+})
+
+
+app.listen(3001, () => {
+    console.log('servidor rodando com sucesso!');
 });
